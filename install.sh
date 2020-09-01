7b9c717f57f7385fefbfbdfe48332d479b0c0a51
Joshua Redmann <joshua.redmann@gmail.com>
2020-09-01 16:35:14 +0200

    remove useless line

--- install.sh
+++ install.sh
@@ -20,7 +20,6 @@ brew cask install iterm2
 brew install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 zsh --version
-upgrade_oh_my_zsh
 brew install git
 brew install vim
 
